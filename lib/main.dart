89117cb46f6fad8a335bafd36314bc6fe27bb502
ShivaShunya <sdfragrance@gmail.com>
2020-04-30 14:10:37 +0530

    Answer question and print in console

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,17 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // void onAnswerQuestion() {
+  //   print('Answered !');
+  // }
+
   @override
   Widget build(BuildContext context) {
+    // var questions = [
+    //   'What color do you like ?',
+    //   'What\'s your favourite animal ?'
+    // ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,13 +26,14 @@ class MyApp extends StatelessWidget {
           ), 
           backgroundColor: Colors.orange,
           ),
-        body: Center(
-          child: IconButton(
-            icon: Icon(Icons.android), 
-            color: Colors.green, 
-            onPressed: () {},
-          ),
-        ),       
+        body: Column(
+          children: [
+            Text('Question ?'),
+            RaisedButton(child: Text('Answer 1'), onPressed: () => print('Answered 1'),),
+            RaisedButton(child: Text('Answer 2'), onPressed: () => print('Answered 2'),),
+            RaisedButton(child: Text('Answer 3'), onPressed: () => print('Answered 3'),),
+          ],
+        ),      
       ),
     );
   }
